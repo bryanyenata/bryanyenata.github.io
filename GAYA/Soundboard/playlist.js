45525e123215b291958865c19ce572a2df72f5f3
bryan-yenata <bryanyenata@yahoo.co.id>
2019-03-05 00:06:18 +0700

    Added GAYA subdir
    For archival purposes

---
+++ GAYA/Soundboard/playlist.js
@@ -0,0 +1,11 @@
+var myPlaylist = [
+
+    {
+        mp3:'audio/celebration.mp3',
+        title:'Celebration',
+        artist:'GAYA 2017',
+        rating:5,
+        duration:'0:30',
+        cover:'mix/1.png'
+    }
+];
